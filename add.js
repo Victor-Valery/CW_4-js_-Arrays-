f8510bbaa4e0d002d792a5689311fbb1b60beb71
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-09 11:45:19 +0200

    js_studies

---
+++ add.js
@@ -0,0 +1,93 @@
+////////////////////////////////
+/* JS Arrays */
+////////////////////////////////
+
+/* var colors = ["Red", "Blue", "Yellow"];  // bu kullanim tarzi tercih edilir.
+
+console.log(colors); */
+
+//----------------------------------------------------------------
+
+
+/* Spaces and line breaks are not important. A declaration can span multiple lines. */
+
+/* var colors = [
+    "Red",
+    "Blue",
+    "Yellow"
+]; */ 
+
+//----------------------------------------------------------------
+
+/* You can also generate an array and assigns values to it in the following example: */
+
+/* var colors = new Array("Red", "Blue", "Yellow"); */
+
+//----------------------------------------------------------------
+
+/* var colors = ["Red", "Blue", "Yellow"];
+var name1 = colors[0];
+console.log(name1); */
+
+//----------------------------------------------------------------
+
+/* Change an element of the array */
+
+/* var colors = ["Red", "Blue", "Yellow"];
+colors[0] ="Green";
+console.log(colors); */
+
+//----------------------------------------------------------------
+
+/* var num = new Array(1,2,3);
+document.write(num[2]); */
+
+//****************************************************************
+
+/* Arrays are Objects */
+
+/* var name = ["John", "James", 24]; */
+
+/* Objects are using names to control their "members". */
+
+/* var name = {firstName:"John", lastName:"James", age:24}; */
+
+//****************************************************************
+
+/* length property */
+
+/* var name2 = ["John" , "James" , "Oliver" , "Aaron" , "Scott"];
+var x = name2.length;          // the length of name is 5
+
+console.log (x); */
+
+//----------------------------------------------------------------
+
+/* var name3 = [];
+var x = name3.length;
+console.log (x); */ // 0
+
+//****************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
